cbb78a2757e6075aeb28f5f571771805e0ef9a98
Ian Kronquist <iankronquist@teleport.com>
2015-04-17 23:14:21 -0700

    Remove configuration functionality from main

--- main.go
+++ main.go
@@ -20,7 +20,11 @@ func main() {
 		os.Exit(EX_USAGE);
 	}
 	verb := arguments[0]
-	config := getConfig()
+	config, err:= timesync.GetConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(EX_USAGE);
+	}
 	if verb == "checkin" {
 		parseCheckIn(config, arguments[1:])
 	} else if verb == "somethingelse" {
@@ -32,52 +36,4 @@ func main() {
 
 }
 
-func getConfig() map[string]string {
-	user := ""
-	if user == "" {
-		user = os.Getenv("USER")
-	}
 
-	editor := os.Getenv("EDITOR");
-	if editor == "" {
-		editor = "vi"
-	}
-
-	config := make(map[string]string)
-	// TODO: parse these fields from config file
-	config["domain"] = "localhost"
-	config["port"] = "3000"
-	config["user"] = user
-	config["editor"] = editor
-	return config
-}
-
-func parseCheckIn(config map[string]string, arguments []string) {
-	flagSet :=flag.FlagSet{}
-
-	checkInData := timesync.CheckIn {
-		Duration: flagSet.Int("duration", 0,
-			"The time spent on the project in minutes, required"),
-		User: flagSet.String("user", "", "The username, required"),
-		Project: flagSet.String("project", "", "The project, required"),
-		Activity: flagSet.String("activity", "", "The activity, required"),
-		Notes: flagSet.String("notes", "", "The notes"),
-		Issue: flagSet.String("issue", "", "A URI for the issue"),
-		Date: flagSet.String("date", "", "The date work was done"),
-	}
-
-	flagSet.Parse(arguments)
-
-	// TODO: validate date fields
-
-	if *checkInData.Duration == 0 || *checkInData.Project == "" ||
-			*checkInData.Activity == "" {
-		fmt.Println("Duration, project, and activity are all required parameters");
-		os.Exit(EX_USAGE);
-	}
-
-	err := timesync.PostCheckIn(config, checkInData)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
