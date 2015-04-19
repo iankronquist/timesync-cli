6e8484c0e754e81abe94dae83231c9a722a1395f
Ian Kronquist <iankronquist@teleport.com>
2015-04-19 10:55:07 -0700

    Simple test of checkin endpoint

--- timesync/checkin_test.go
+++ timesync/checkin_test.go
@@ -1,1 +1,61 @@
 package timesync
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"bytes"
+)
+
+func Test_PostCheckIn_200(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			expectedURL := "/checkin/add"
+			if r.URL.String() != expectedURL {
+				t.Error("Unexpected URL", r.URL.String(),
+					"expected", expectedURL)
+			}
+			bodyData, err := ioutil.ReadAll(r.Body)
+			defer r.Body.Close()
+			if err != nil {
+				t.Error("Error reading body")
+			}
+			expectedBodyData := []byte("{\"duration\":12,\"user\":\"iankronquist\",\"project\":\"ww\",\"activity\":\"review\",\"notes\":\"reviewed PR #14\",\"issue_uri\":\"github.com/osu-cass/whats_fresh_api/pulls/14\",\"date\":\"2015-04-18\"}");
+
+			if bytes.Compare(bodyData, expectedBodyData) != 0 {
+				t.Error("Request body did not match. Got: ", string(bodyData),
+					"expected", string(expectedBodyData))
+			}
+		}))
+	defer ts.Close()
+
+	duration := 12
+	user := "iankronquist"
+	activity := "review"
+	notes := "reviewed PR #14"
+	project := "ww"
+	issue := "github.com/osu-cass/whats_fresh_api/pulls/14"
+	date := "2015-04-18"
+
+	checkInData := CheckIn {
+		Duration: &duration,
+		User: &user,
+		Activity: &activity,
+		Notes: &notes,
+		Project: &project,
+		Issue: &issue,
+		Date: &date,
+	}
+
+	config := Config {
+		URL: ts.URL + "/checkin/add",
+	}
+
+	err := PostCheckIn(config, checkInData)
+
+	if err != nil {
+		t.Error("Error in PostCheckIn", err)
+	}
+}
